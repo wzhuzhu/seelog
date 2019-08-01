4497605ba130d38be3aa64c5a3feb07740b84de4
Anthony.liu <anthony.liu@wiwide.com>
2019-08-01 14:35:52 +0800

    从日志末尾开始，解决log文件很大时的无用消耗。

--- monitor.go
+++ monitor.go
@@ -3,17 +3,18 @@ package seelog
 import (
 	"github.com/hpcloud/tail"
 	"log"
+	"os"
 )
 
 type msg struct {
 	LogName string `json:"logName"`
-	Data	string `json:"data"`
+	Data    string `json:"data"`
 }
 
 // 监控日志文件
 func monitor() {
 
-	for _,sl := range slogs {
+	for _, sl := range slogs {
 		go func(sl slog) {
 			defer func() {
 				if err := recover(); err != nil {
@@ -21,11 +22,20 @@ func monitor() {
 				}
 			}()
 
-			log.Println("开始进行日志监控",sl.Name, sl.Path)
+			log.Println("开始进行日志监控", sl.Name, sl.Path)
 
-			t, _ := tail.TailFile(sl.Path, tail.Config{Follow: true})
+			file, err := os.Open(sl.Path)
+			if err != nil {
+				log.Printf("[seelog} error:%+v", err)
+			}
+			fileInfo, _ := file.Stat()
+			t, _ := tail.TailFile(sl.Path, tail.Config{Follow: true, Location: &tail.SeekInfo{
+				Offset: fileInfo.Size(),
+				Whence: 0,
+			}})
+			file.Close()
 			for line := range t.Lines {
-				manager.broadcast <- msg{sl.Name,line.Text}
+				manager.broadcast <- msg{sl.Name, line.Text}
 			}
 		}(sl)
 	}
